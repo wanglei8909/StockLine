98f0c4aea9a5eba69b16fdcb95f5a6419b08bcea
wanglei <wanglei@9086.cn>
2017-07-19 15:05:03 +0800

    Initial Commit

---
+++ StockLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StockLine
+//
+//  Created by wanglei on 2017/7/19.
+//  Copyright © 2017年 wanglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
